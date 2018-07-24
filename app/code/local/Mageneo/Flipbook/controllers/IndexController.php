e5e8b8e9d3641d11b6dd54ed2bfb9d99e317eebf
Andrii Beziazychnyi <andrewbess@artjoker.net>
2018-07-24 09:43:49 +0300

    Temporary fixes of the flipbook scripts.

--- app/code/local/Mageneo/Flipbook/controllers/IndexController.php
+++ app/code/local/Mageneo/Flipbook/controllers/IndexController.php
@@ -6,6 +6,16 @@ class Mageneo_Flipbook_IndexController extends Mage_Core_Controller_Front_Action
     {
         $this->loadLayout();
         $layout = $this->getLayout();
+        /**
+         * @var $headBlock Mage_Page_Block_Html_Head
+         */
+        $headBlock = $layout->getBlock('head');
+        $headBlock->addJs('catalogue/extras/jquery-migrate-1.2.1.js')
+            ->addJs('catalogue/extras/jquery-ui-1.8.20.custom.min.js')
+            ->addJs('catalogue/extras/modernizr.2.5.3.min.js')
+            ->addJs('lib/jquery/noconflict.js')
+            ->addJs('catalogue/lib/hash.js');
+
         $this->renderLayout();
     }
 }
